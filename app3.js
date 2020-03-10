e81e87886a41044a1d78d9922e47828c66d739da
jaassmine <jasminesharma0298@gmail.com>
2020-03-10 17:45:28 +0530

    Data Type in Js

---
+++ app3.js
@@ -0,0 +1,31 @@
+// varables with diiferent types 
+//PRIMITIVE 
+
+//String 
+const name = 'Andrew';
+// Number
+const age = 45 ;
+// Boolean 
+const married = true ;
+// Null
+const car = null ;
+// Undefined
+let test ;
+// Symbol 
+const sym = Symbol();
+console.log(typeof sym);
+
+
+// REFERENCE TYPES - Objects
+// Array 
+const hobies = ['movies', 'music'];
+// Object literal 
+const  address = {
+    city: 'Boston',
+    state: 'MA'
+}
+
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
