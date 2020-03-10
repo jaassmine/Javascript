5d3e9cfdd5a6bceeb98438dbcd6e95e9622a28ad
jaassmine <jasminesharma0298@gmail.com>
2020-03-10 18:02:08 +0530

    console log

---
+++ fundamentals/app1.js
@@ -0,0 +1,35 @@
+//log to console
+
+console.log('hello World');
+
+//for Number
+console.log(1234321);
+//for boolean value
+console.log(true);
+
+//for variable
+var greet = 'hello ';
+console.log(greet);
+
+//for array
+console.log([1,20,30,4]);
+console.log({a:1 , b:2});
+console.table({a:1,b:2});
+
+//for clearing console window
+console.clear();
+
+//for error
+console.error('This is Error');
+
+//for warning
+console.warn('this is Warning');
+//for time calculating time
+console.time('hello ');
+    console.log('hello World');
+    console.log('hello World');
+    console.log('hello World');
+    console.log('hello World');
+    console.log('hello World');
+//for ending time
+console.timeEnd('hello ');
