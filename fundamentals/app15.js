3d7cae8404cc0ee0ec1b769c2b824d54652531e1
jaassmine <jasminesharma0298@gmail.com>
2020-03-15 22:32:08 +0530

    A Look At The Window Object

---
+++ fundamentals/app15.js
@@ -0,0 +1,58 @@
+// WINDOE METHODS OBJECT
+window.console.log(1213);
+
+// //ALERT
+// window.alert('Hello World');
+
+////PROMPT
+// const input = prompt();
+// alert(input);
+
+////CONFIRM
+// if(confirm('are you sure '))
+// {
+//    console.log('YES');
+// }
+// else
+//    console.log('NO');
+
+let val ;
+//OUTER HEIGHT AND WIDTH
+val = window.outerHeight;
+val = window.outerWidth;
+
+//INNER HEIGHT AND WIDTH
+val = window.innerHeight;
+val = window.innerWidth;
+
+//SCOLL POINTS
+val = window.scrollY;
+val = window.scrollX;
+
+//LOCATION OBJECT
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// // REIRECT 
+// window.location.href = 'http://google.com';
+////RELOAD 
+//window.location.reload();
+
+//HISTORY OBJECT
+//window.history.go();
+val = window.history.length;
+
+
+//Navigator object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.platform
+val = window.navigator.userAgent;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+console.log(val);
